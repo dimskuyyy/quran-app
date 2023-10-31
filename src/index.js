4ac8275a2204169bdb6274eccc812aef57746834
dimskuyyy <dimasfauzan1712@gmail.com>
2023-10-31 20:02:31 +0700

    deploy

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "regenerator-runtime";
-import "./style/style.css";
-import "./script/component/surat-list.js";
-import "./script/component/ayat-list.js";
-import main from "./script/view/main.js";
-
-document.addEventListener("DOMContentLoaded", main);
--- src/script/setup/twind.js
+++
@@ -1,6 +0,0 @@
-import {create,cssomSheet} from "twind";
-
-const sheet = cssomSheet({target:new CSSStyleSheet()});
-const {tw} = create({sheet});
-
-export {sheet,tw};
